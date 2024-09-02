be59360563b81ce5a768ee11ef7b93e4434d60c2
eaemorgado <eaemorgado@gmail.com>
2024-09-02 19:50:14 -0300

    att

--- js/pop-empresa.js
+++ js/pop-empresa.js
@@ -1,7 +1,36 @@
-function showPopup(id) {
-  document.getElementById(id).style.display = 'block';
-}
+// function showPopup(id) {
+//   document.getElementById(id).style.display = 'block';
+// }
 
-function hidePopup(id) {
-  document.getElementById(id).style.display = 'none';
-}
+// function hidePopup(id) {
+//   document.getElementById(id).style.display = 'none';
+// }
+
+document.addEventListener('DOMContentLoaded', function() {
+  function showPopup(popup) {
+    popup.classList.add('show');
+  }
+
+  function hidePopup(popup) {
+    popup.classList.remove('show');
+  }
+
+  const logoContainers = document.querySelectorAll('.logo-container');
+
+  logoContainers.forEach(container => {
+    const popupId = container.getAttribute('data-popup-id');
+    const popup = document.getElementById(popupId);
+
+    container.addEventListener('mouseover', () => showPopup(popup));
+    container.addEventListener('mouseout', () => hidePopup(popup));
+
+    // Para dispositivos móveis, usamos toque
+    container.addEventListener('touchstart', () => {
+      if (popup.classList.contains('show')) {
+        hidePopup(popup);
+      } else {
+        showPopup(popup);
+      }
+    });
+  });
+});
